43e5d8fa0ca50aad31f18c37392a46d530a2cc26
Andre Foo <andreg.foo@gmail.com>
2022-11-26 22:45:40 +0800

    i did... something

--- app/src/main/java/com/example/assignment/Models/Movie.kt
+++ app/src/main/java/com/example/assignment/Models/Movie.kt
@@ -1,9 +1,10 @@
 package com.example.assignment.models
 
-class Movie(
-	var title: String,
-	var description: String,
-	var language: String,
-	var releaseDate: String,
-	var suitableForChildren: Boolean
-)
+class Movie(var title: String,
+            var description: String,
+            var releaseDate: String,
+            var violence: Boolean = false,
+            var languageUsed: Boolean = false,
+            var language: Language = Language.ENGLISH)
+
+enum class Language { ENGLISH, CHINESE, MALAY, TAMIL }
